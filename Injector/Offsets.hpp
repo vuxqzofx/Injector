4f945d9ad1abdb64acd8eb9c3547182b1cc40855
vuxqzofx <liasyour@icloud.com>
2026-01-23 19:40:52 +0100

    Update Offsets.hpp

--- Injector/Offsets.hpp
+++ Injector/Offsets.hpp
@@ -3,8 +3,8 @@
 typedef unsigned long long uintptr_t;
 
 namespace Offsets {
-    inline uintptr_t TaskSchedulerPointer = 0x8056DC8;
+    inline uintptr_t TaskSchedulerPointer = 0x8057E48;
     inline uintptr_t TaskSchedulerToJobs = 0x1D0;
 
-    inline uintptr_t NtUnmapViewOfSectionSyscall = 0xD962B8;
+    inline uintptr_t NtUnmapViewOfSectionSyscall = 0xADDF95;
 }
